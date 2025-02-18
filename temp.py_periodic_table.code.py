80eec3ac8568bd8eea2bfd919d9a9b9a527556e4
Lucifer155 <jeansniperz950@gmail.com>
2025-02-18 11:14:05 +0900

    Add files via upload
    Periodic Table but 20 or under Atomic Number only prototype for app

---
+++ temp.py_periodic_table.code.py
@@ -0,0 +1,99 @@
+
+
+
+
+import sys
+print(sys.executable)
+
+import streamlit as st
+
+
+
+
+
+st.title("Under 20 atomic number Periodic Table prototype")
+
+import streamlit as st
+
+name = st.text_input("Enter your name:")
+if st.button("Submit"):
+    st.write(f"Hello, {name}!")
+print("This is a prototype for my periodic table app and can tell you the atomic mass and the amount of protons, neutrons, and electrons!")
+
+
+element = input("What Element do you want to know about?")
+
+
+if element == "Hydrogen":
+    print("Hydrogen's atomic number is 1 consisting of 1 Electron, 1 Proton, and 1 Neutron. It's atomic mass is 1.")
+
+
+elif element =="Helium":
+    print("Helium has a atomic number of 2 and consists of 2 Electrons, 2 Protons, and 2 Neutrons. It's atomic mass is 4")
+
+
+elif element =="Lithium":
+    print("Lithium has a atomic number of 3 and consists of 3 Electrons, 3 Protons, and 3 Neutrons. It's atomic mass is 6")
+
+
+elif element =="Beryllium":
+    print("Beryllium has a atomic number of 4 and consists of 4 Electrons, 4 Protons, and 4 Neutrons. It's atomic mass is 8")
+
+
+elif element =="Boron":
+    print("Boron has a atomic number of 5 and consists of 5 Electrons, 5 Protons, and 5 Neutrons. It's atomic mass is 10")
+    
+elif element =="Carbon":
+    print("Carbon has a atomic number of 6 and consists of 6 Electrons, 6 Protons, and 6 Neutrons. It's atomic mass is 12")
+    
+elif element =="Nitrogen":
+     print("Nitrogen has a atomic number of 7 and consists of 7 Electrons, 7 Protons, and 7 Neutrons. It's atomic mass is 14")
+    
+elif element =="Oxygen":
+    print("Oxygen has a atomic number of 8 and consists of 8 Electrons, 8 Protons, and 8 Neutrons. It's atomic mass is 16")
+    
+elif element =="Fluorine":
+    print("Fluorine has a atomic number of 9 and consists of 9 Electrons, 9 Protons, and 9 Neutrons. It's atomic mass is 18")
+    
+elif element =="Neon":
+    print("Neon has a atomic number of 10 and consists of 10 Electrons, 10 Protons, and 10 Neutrons. It's atomic mass is 20")
+    
+elif element =="Sodium":
+    print("Sodium has a atomic number of 11 and consists of 11 Electrons, 11 Protons, and 11 Neutrons. It's atomic mass is 22")
+    
+elif element =="Magnesium":
+    print("Magnesium has a atomic number of 12 and consists of 12 Electrons,12 Protons, and 12 Neutrons. It's atomic mass is 24")
+    
+elif element =="Aluminium":
+    print("Aluminium has a atomic number of 13 and consists of 13 Electrons, 13 Protons, and 13 Neutrons. It's atomic mass is 26")
+    
+elif element =="Silicon":
+    print("Silicon has a atomic number of 14 and consists of 14 Electrons, 14 Protons, and 14 Neutrons. It's atomic mass is 28")
+    
+elif element =="Phosphorus":
+    print("Phosphorus has a atomic number of 15 and consists of 15 Electrons, 15 Protons, and 1 Neutrons. It's atomic mass is 30")
+    
+elif element =="Sulfur":
+    print("Sulfur has a atomic number of 16 and consists of 16 Electrons, 16 Protons, and 16 Neutrons. It's atomic mass is 32")
+    
+elif element =="Chlorine":
+    print("Chlorine has a atomic number of 17 and consists of 17 Electrons, 17 Protons, and 17 Neutrons. It's atomic mass is 34")
+    
+elif element =="Argon":
+    print("Argon has a atomic number of 18 and consists of 18 Electrons, 18 Protons, and 18 Neutrons. It's atomic mass is 36")
+    
+elif element =="Potassium":
+    print("Potassium has a atomic number of 19 and consists of 19 Electrons, 19 Protons, and 19 Neutrons. It's atomic mass is 38")
+    
+elif element =="Calcium":
+    print("Calcium has a atomic number of 20 and consists of 20 Electrons, 20 Protons, and 20 Neutrons. It's atomic mass is 40")
+
+    print("Oxygen has a atomic number of 8 and consists of 8 Electrons, 8 Protons, and 8 Neutrons. It's atomic mass is 16")
+else: 
+    print("I apologize but I am programmed to answer elements under 20 atomic number")
+
+    
+
+    
+ 
+    
